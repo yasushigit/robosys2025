f96ce490329860f49119e6932fdccc194aad1e2b
Yasushi Ando <gt4syabatto@gmail.com>
2025-10-30 14:22:53 +0900

    iikanji

--- test.bash
+++ test.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2025 Yasushi Ando 　　　　　
-# SPDX-License-Identifier: GPL-3.0-only
+#SPDX-FileCopyrightText: 2025 Yasushi Ando 　　　　　
+#SPDX-License-Identifier: GPL-3.0-only
 
 ng() {
-    echo "${1}行目が違うよ"
-    res=1
+	echo "${1}行目が違うよ"
+	res=1
 }
 
 res=0
@@ -12,5 +12,5 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo "OK"
-exit "$res"
+[ "${res}" = 0 ] && echo OK
+exit $res
